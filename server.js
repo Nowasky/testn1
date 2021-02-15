a8a2083e1370834ee078a7aa15cc53e956e11079
root <root@localhost.localdomain>
2021-02-15 12:53:54 -0500

    b2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 const WebSocket = require('ws')
 
-var client = '';
+var global.client;
 
 //const wss = new WebSocket.Server({ port: 8080 })
 //const wss = new WebSocket.Server({ server })
@@ -25,7 +25,7 @@ app.get("/", (req,res) => {
 
 app.post("/report", (req,res) => {
   console.log(req.body)
-  //client.send('CSP report: ' + req.body);
+  global.client.send('CSP report: ' + req.body);
   return res.send('CSP violation report received');
 });
 
@@ -41,9 +41,9 @@ const wss = new WebSocket.Server({ server });
 //const wss = new WebSocket.Server({ port: 8080 })
 //const wss = new WebSocket.Server({ server })
 wss.on('connection', ws => {
-  client = ws;
+  global.client = ws;
   //ws.on('message', message => {
   //  console.log(`Received message => ${message}`)
   //})
-  client.send('Hello! Message From Server!!');
+  global.client.send('Hello! Message From Server!!');
 })
